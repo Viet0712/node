4c7e6d087d572fcc544c946090f46cdaf025138b
Viet0712 <129593801+Viet0712@users.noreply.github.com>
2024-03-13 09:45:27 +0700

    first commit

---
+++ controller/Addnew_controller.js
@@ -0,0 +1,52 @@
+class AddNew_controller {
+  add_new(req, res) {
+    res.render("form", { a: true });
+  }
+
+  add_newPost(req, res) {
+    const path = require("path");
+    const multer = require("multer");
+
+    var storage = multer.diskStorage({
+      destination: function (req, file, cb) {
+        cb(null, path.join(__dirname, "../public/uploads"));
+      },
+      filename: function (req, file, cb) {
+        const ext = path.extname(file.originalname);
+        cb(null, req.body._id + ext);
+      },
+    });
+    const upload = multer({ storage: storage });
+
+    upload.single("image")(req, res, async function (err) {
+      //   console.log(req.file);
+
+      //   console.log(req.body._id);
+      if (err) {
+        return res.status(500).json({ message: "Error uploading file" });
+      }
+      let image;
+      if (req.file) {
+        image = req.file.filename;
+        // console.log(image);
+      }
+      try {
+        const Product_model = require("../models/product_models.js");
+        if (req.body.description == "") {
+          req.body.description = "Không Có";
+        }
+        Product_model.create(req.body);
+        await Product_model.findByIdAndUpdate(req.body._id, {
+          image: image || undefined, // Sử dụng image nếu có, nếu không thì là undefined
+        });
+        req.session.status = true;
+        res.redirect("/?status=add");
+      } catch (error) {
+        console.log(error.log);
+        res.status(500).json({ message: error.message });
+      }
+    });
+  }
+}
+
+module.exports = new AddNew_controller();
